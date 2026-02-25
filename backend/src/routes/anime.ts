dfeecab1fb8064456d41ef47cdb26df45d7a0ac9
LuchooLopez <luchoolopez2810@gmail.com>
2026-02-24 23:08:30 -0300

    creado anime router

---
+++ backend/src/routes/anime.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { AnimeController } from "../controllers/anime.controller";
+import { AnimeService } from "../services/anime.service";
+
+const animeRouter = Router();
+const animeService = new AnimeService();
+const animeController = new AnimeController(animeService);
+
+animeRouter.post("/", animeController.createAnime);
+animeRouter.get("/:usuarioId", animeController.getAnimesByUserId);
+animeRouter.put("/:animeId", animeController.updateAnime);
+animeRouter.delete("/:animeId", animeController.deleteAnime);
+animeRouter.patch("/:animeId/capitulo", animeController.updateCapituloEstado);
+
+export default animeRouter;
+export { animeRouter as router };
